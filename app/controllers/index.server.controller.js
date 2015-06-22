237de808346f54498f82d0f1e7557cba152efb83
Guang Hu <guanghu@admins-MacBook-Pro.local>
2015-06-22 14:14:24 -0500

    adding group

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -16,12 +16,15 @@ var projectInfo = mysql.createConnection(
     }
 );
 
+
 var queryString = 'SELECT * FROM PROJECT_INFO';
+
 
 var base = function(req, res) {
     res.render('index', {
         title: 'Howdy World'})
 };
+
 
 var insert = function(req, res) {
 
@@ -41,8 +44,10 @@ var insert = function(req, res) {
             //the columns in the table are provided below
             var costs = [], dates = [], mandates = [];
 
-            if (rows.length == 0)
+            if (rows.length == 0) {
                 res.render('insert', {rows: rows, weight: weight});
+                return;
+            }
 
             // convert rows to int and iterate through
             for (var row in rows) {
@@ -52,7 +57,8 @@ var insert = function(req, res) {
                 //console.log("date datatype is " + typeof rows[row].MANDATE);
                 console.log("date:" + rows[row].DATE.getTime());
                 //convert DATEs to integers
-                rows[row].DATE_REL = ((new Date().getTime() - rows[row].DATE.getTime()));
+                rows[row].DATE_REL = modifyDate((rows[row].DATE.getTime() - new Date().getTime()));
+                console.log('date_rel is ' + rows[row].DATE_REL + '     date is ' + rows[row].DATE);
                 dates.push(rows[row].DATE_REL);
                 mandates.push(rows[row].MANDATE);
             }
@@ -72,7 +78,11 @@ var insert = function(req, res) {
             console.log("std = " + costStd + ', ' + dateStd + ', ' + mandateStd);
             console.log("mean = " + costMean + ',' + dateMean + ',' + mandateMean);
 
+            console.log("weight = " + weight.Cost + ',' + weight.Date + ',' + weight.Mandate);
             rows.sort(function(a, b) {
+                if (b.Group > a.Group)
+                    return 1;
+                else return -1;
                 var f = function(a) {
                     //var w = a.COST * weight.Cost + a.DATE * weight.Date + a.MANDATE * weight.Mandate;
                     //console.log(a.PROJECT_NAME + " priority is " + w);
@@ -80,9 +90,10 @@ var insert = function(req, res) {
                         if (data == mean) return 0;
                         return (data - mean) / std;
                     };
-                    console.log('date is ' + a.Date);
+
                     console.log("zscore of cost = " + z_score(a.COST, costMean, costStd) + " zscore of date = " + z_score(a.DATE_REL, dateMean, dateStd) +" zscore of mandate = " + z_score(a.MANDATE, mandateMean, mandateStd));
-                    var w = z_score(a.COST, costMean, costStd) * weight.Cost + z_score(a.DATE_REL, dateMean, dateStd) * weight.Date + z_score(a.MANDATE, mandateMean, mandateStd * weight.Mandate);
+                    var w = z_score(a.COST, costMean, costStd) * weight.Cost + z_score(a.DATE_REL, dateMean, dateStd) * weight.Date + z_score(a.MANDATE, mandateMean, mandateStd) * weight.Mandate;
+
                     console.log("w = " + w);
                     return w;
                 };
@@ -94,6 +105,7 @@ var insert = function(req, res) {
     });
 };
 
+
 var update = function(req, res) {
 
 
@@ -103,6 +115,7 @@ var update = function(req, res) {
     post.Cost = req.body.Cost;
     post.Date = req.body.Date;
     post.Mandate = req.body.Mandate;
+    post.Group = req.body.Group;
     console.log('mandate = ' +  post.Mandate);
 
     var query = projectInfo.query('INSERT INTO project_info set ?', post, function(err, result) {
@@ -132,6 +145,7 @@ var refresh = function(req, res) {
     });
 };
 
+
 var del = function(req, res) {
     console.log(req.query.ProjectName);
     var delQuery = 'DELETE FROM PROJECT_INFO WHERE PROJECT_NAME =' + '\'' + req.query.ProjectName + '\'';
@@ -142,7 +156,13 @@ var del = function(req, res) {
     })
 };
 
-
+var modifyDate = function(num) {
+    var days = num / 1000 / 60 / 60 / 24;
+    if (days <= 1) {
+        return 3;
+    }
+    else return 0 - math.log(days / 1000, 10);
+};
 
 exports.base = base;
 exports.insert = insert;
--- testServer.js
+++ testServer.js
@@ -3,9 +3,13 @@
  */
 var http = require("http");
 var math = require('mathjs');
+var java = require('java');
 http.createServer(function(request, response) {
     var m = math.mean(1, 2);
+
     console.log(m);
+    var javaLong = java.newInstanceSync("java.lang.Long", 5);
+    console.log('Possibly truncated long value: ' + javaLong);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
